b108da522d8c8f62cc7236f72794ee0e3d8d6d7d
Diego Paraguai <107950626+dihparaguai@users.noreply.github.com>
2024-07-09 12:52:33 -0300

    Create Lista_Registro.m

---
+++ Lista_Registro.m
@@ -0,0 +1,6 @@
+// Lista Registro
+let
+    registro = [Nome = "Alura", Idade = 15],
+    lista_registro = {[Nome = "Alura", Idade = 15], [Nome = "Power", Idade = 20], [Nome = "Query", Idade = 10]}
+in
+    lista_registro
