e4852eb4d9f63051eee0c50a6cf37a4c9683e7ec
Diego Paraguai <107950626+dihparaguai@users.noreply.github.com>
2024-07-09 12:44:07 -0300

    Create Variaveis.m

---
+++ Variaveis.m
@@ -0,0 +1,14 @@
+// Variaveis
+let
+    var_1 =
+        let
+            a = "POWER"
+        in
+            a,
+    var_2 = 
+        let
+            b = "QUERY"
+        in
+            b
+in
+    var_1 & " " & var_2
