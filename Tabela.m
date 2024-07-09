ec2d294ae902d145871fa6cc848e6d94342523e2
Diego Paraguai <107950626+dihparaguai@users.noreply.github.com>
2024-07-09 12:53:18 -0300

    Create Tabela.m

---
+++ Tabela.m
@@ -0,0 +1,20 @@
+// Tabela
+// Nao precisa de "let" e "in" para criar tabelas
+
+#table(
+    // lista com os nomes das colunas
+    {
+        "nome", "data", "idade"
+    },
+    // lista com a lista de valores de cada campo
+    {
+        // linha 1
+        {
+            "alura", "01/01/2022", 20
+        },
+        // linha 2
+        {
+            "power bi", "01/10/2023", 30
+        }
+    }
+)
