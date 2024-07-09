84d798e6c5f0462ea4a520f05e5d36683a88167d
Diego Paraguai <107950626+dihparaguai@users.noreply.github.com>
2024-07-09 12:59:05 -0300

    Create Funcao_com_Parametros_Tipados.m

---
+++ Funcao_com_Parametros_Tipados.m
@@ -0,0 +1,9 @@
+// Função com Parametros Tipados
+(a as number, b as number) => 
+    a + b
+
+// Função com Parametros Tipados Invocada
+let
+    Fonte = #"Função com Parametros Tipados"(5, 6)
+in
+    Fonte
